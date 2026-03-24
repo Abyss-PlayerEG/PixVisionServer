c3868fc98086cb43c3d1fa48a6be36c0967f11ed
PlayerEG <enderg@qq.com>
2026-03-24 14:14:41 +0800

    refactor(docs): 将 Swagger 注解从 V2 升级到 V3 并更新相关依赖
    - 将 io.swagger.annotations 替换为 io.swagger.v3.oas.annotations
    - 更新 Controller 中的 @Api、@ApiOperation、@ApiParam 注解为 @Tag、@Operation、@Parameter
    - 更新 POJO 类中的 @ApiModel、@ApiModelProperty 注解为 @Schema
    - 在 pom.xml 中将 Springfox 依赖替换为 SpringDoc OpenAPI
    - 更新 MyBatis 相关依赖版本
    - 移除过时的依赖管理配置
    - 优化 Swagger 配置类以使用新的 OpenAPI 规范

--- src/main/java/top/playereg/pix_vision/pojo/SystemInfo.java
+++ src/main/java/top/playereg/pix_vision/pojo/SystemInfo.java
@@ -1,7 +1,6 @@
 package top.playereg.pix_vision.pojo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,64 +8,64 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ApiModel("系统相关信息")
+@Schema(description = "系统相关信息")
 public class SystemInfo {
-    @ApiModelProperty(value = "获取时间", required = true)
+    @Schema(description = "获取时间", required = true)
     private String nowTime;
-    @ApiModelProperty(value = "操作系统架构")
+    @Schema(description = "操作系统架构")
     private String osArch;
-    @ApiModelProperty(value = "操作系统名称")
+    @Schema(description = "操作系统名称")
     private String osName;
-    @ApiModelProperty(value = "操作系统版本")
+    @Schema(description = "操作系统版本")
     private String osVersion;
-    @ApiModelProperty(value = "用户名")
+    @Schema(description = "用户名")
     private String userName;
-    @ApiModelProperty(value = "用户主目录")
+    @Schema(description = "用户主目录")
     private String userHome;
-    @ApiModelProperty(value = "项目目录")
+    @Schema(description = "项目目录")
     private String userCurrentDir;
-    @ApiModelProperty(value = "用户临时目录")
+    @Schema(description = "用户临时目录")
     private String userTempDir;
-    @ApiModelProperty(value = "用户语言")
+    @Schema(description = "用户语言")
     private String userLanguage;
-    @ApiModelProperty(value = "用户国家")
+    @Schema(description = "用户国家")
     private String userCountry;
-    @ApiModelProperty(value = "主机名")
+    @Schema(description = "主机名")
     private String hostName;
-    @ApiModelProperty(value = "主机地址")
+    @Schema(description = "主机地址")
     private String hostAddress;
-    @ApiModelProperty(value = "最大内存")
+    @Schema(description = "最大内存")
     private String maxMemory;
-    @ApiModelProperty(value = "总内存")
+    @Schema(description = "总内存")
     private String totalMemory;
-    @ApiModelProperty(value = "空闲内存")
+    @Schema(description = "空闲内存")
     private String freeMemory;
-    @ApiModelProperty(value = "可用内存")
+    @Schema(description = "可用内存")
     private String usableMemory;
-    @ApiModelProperty(value = "Java虚拟机名称")
+    @Schema(description = "Java 虚拟机名称")
     private String jvmName;
-    @ApiModelProperty(value = "Java虚拟机版本")
+    @Schema(description = "Java 虚拟机版本")
     private String jvmVersion;
-    @ApiModelProperty(value = "Java虚拟机供应商")
+    @Schema(description = "Java 虚拟机供应商")
     private String jvmVendor;
-    @ApiModelProperty(value = "Java虚拟机信息")
+    @Schema(description = "Java 虚拟机信息")
     private String jvmInfo;
-    @ApiModelProperty(value = "Java虚拟机规范名称")
+    @Schema(description = "Java 虚拟机规范名称")
     private String jvmSpecName;
-    @ApiModelProperty(value = "Java虚拟机规范版本")
+    @Schema(description = "Java 虚拟机规范版本")
     private String jvmSpecVersion;
-    @ApiModelProperty(value = "Java虚拟机规范供应商")
+    @Schema(description = "Java 虚拟机规范供应商")
     private String jvmSpecVendor;
-    @ApiModelProperty(value = "JDK版本")
+    @Schema(description = "JDK 版本")
     private String jdkVersion;
-    @ApiModelProperty(value = "JDK供应商")
+    @Schema(description = "JDK 供应商")
     private String jdkVendor;
-    @ApiModelProperty(value = "JDK供应商URL")
+    @Schema(description = "JDK 供应商 URL")
     private String jdkVendorURL;
-    @ApiModelProperty(value = "JDK规范名称")
+    @Schema(description = "JDK 规范名称")
     private String jdkSpecName;
-    @ApiModelProperty(value = "JDK规范版本")
+    @Schema(description = "JDK 规范版本")
     private String jdkSpecVersion;
-    @ApiModelProperty(value = "JDK规范供应商")
+    @Schema(description = "JDK 规范供应商")
     private String jdkSpecVendor;
 }
--- src/main/java/top/playereg/pix_vision/pojo/User.java
+++ src/main/java/top/playereg/pix_vision/pojo/User.java
@@ -1,7 +1,6 @@
 package top.playereg.pix_vision.pojo;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,34 +10,34 @@ import java.sql.Timestamp;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ApiModel("用户实体")
+@Schema(description = "用户实体")
 public class User {
 
-    @ApiModelProperty( value = "用户ID" )
+    @Schema( description = "用户 ID")
     Integer user_id;
-    @ApiModelProperty( value = "用户UUID" )
+    @Schema( description = "用户 UUID")
     Byte user_uuid;
-    @ApiModelProperty( value = "用户名")
+    @Schema( description = "用户名")
     String username;
-    @ApiModelProperty( value = "用户密码")
+    @Schema( description = "用户密码")
     String password;
-    @ApiModelProperty( value = "用户昵称" )
+    @Schema( description = "用户昵称" )
     String nickname;
-    @ApiModelProperty( value = "用户头像路径")
+    @Schema( description = "用户头像路径")
     String avatar_url; //用户头像
-    @ApiModelProperty( value = "绑定邮箱" )
+    @Schema( description = "绑定邮箱" )
     String email;
-    @ApiModelProperty( value = "删除标签")
+    @Schema( description = "删除标签")
     Boolean is_delete;
-    @ApiModelProperty( value = "账户状态")
+    @Schema( description = "账户状态")
     Integer status;
-    @ApiModelProperty( value = "更新人员")
+    @Schema( description = "更新人员")
     Timestamp update_time; //更新时间戳
-    @ApiModelProperty( value = "更新人员ID" )
-    Integer update_user; //更新人员ID
-    @ApiModelProperty( value = "创建时间" )
+    @Schema( description = "更新人员 ID" )
+    Integer update_user; //更新人员 ID
+    @Schema( description = "创建时间" )
     Timestamp create_time;
-    @ApiModelProperty( value = "创建人员ID" )
-    Integer create_user; //创建人员ID
+    @Schema( description = "创建人员 ID" )
+    Integer create_user; //创建人员 ID
 
 }
